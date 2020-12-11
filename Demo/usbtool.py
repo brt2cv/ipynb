955233dc734b3225c2d174fa808fc3012319b338
Bright@E49 <brt2@qq.com>
2020-12-11 19:10:04 +0800

    提供usbtool，用于检测图像处理脚本；适配树霉派OpenCV3.2.0版本

--- Demo/usbtool.py
+++ Demo/usbtool.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-# @Date    : 2020-12-04
+# @Date    : 2020-12-09
 # @Author  : Bright Li (brt2@qq.com)
 # @Link    : https://gitee.com/brt2
-# @Version : 0.2.2
+
 
 from importlib import reload
 import traceback
@@ -11,42 +11,28 @@ from pyqt.qtwx import *
 from pyqt.qtcv import *
 from pycv.camera import Qt5Camera
 import pycv.opencv as cv
-from ocr import OcrEngineMixin
 
 import script
 
-TessEnv = {
-    # "TesseractBinPath": "D:/programs/Tesseract",
-    # "TessDataDir": "/home/brt/ws/ipynb/tutorial/tesseract/tessdata",
-    "TessDataDir": "D:/Home/workspace/ipynb/tutorial/tesseract/tessdata",
-    "Lang": "eng"
-}
 
-class MainWnd(OcrEngineMixin, BaseCvWnd):
-    statusbar_msg = '请移动画面，将字符置于识别框中'
-
+class MainWnd(BaseCvWnd):
     def __init__(self, parent, camera_idx=0, solution=None, isRGB=False):
         super().__init__(parent, camera_idx, solution, isRGB)
         loadUi("ui/wx_mwnd_with_ctrllers.ui", self)
         super()._setup_ui()
-
-        self.setWindowTitle("OCR字符识别")
-        # self.set_roi([250, 200, 300, 100])
-        self.ROI = True
-
-        self.ocr_init(TessEnv["TessDataDir"], TessEnv["Lang"])
+        self.setWindowTitle("OpenCV 图像处理")
 
     def update_script(self):
         """ 由于关乎可变脚本script，故需要在子类重写 """
         reload(script)
-        self.status_bar.showMessage("预处理脚本已更新")
         self.define_improc()
+        self.status_bar.showMessage("预处理脚本已更新")
+
 
     def define_improc(self):
         """ 由于关乎可变脚本script，故需要在子类重写 """
         self.improc_methods = {
-            "window2": script.improc_ocr,  # make_right
-            "parse_roi": script.improc_ocr,
+            "window2": script.improc_roi,  # make_right
         }
 
     def ocr_result(self, result):
@@ -68,13 +54,6 @@ class MainWnd(OcrEngineMixin, BaseCvWnd):
             traceback.print_exc()
             im_right = im_left
 
-        # 绘制ROI区域
-        if self.ROI:
-            func = self.improc_methods.get("parse_roi")
-            if func:
-                im_text = func(im_left, *list_params)
-                self.ocr_exec(im_text)
-
         if self.isSwitched:
             im_left, im_right = im_right, im_left
 
@@ -85,5 +64,3 @@ class MainWnd(OcrEngineMixin, BaseCvWnd):
 
 # run_qtapp(SimpleOCR)
 run_qtapp(MainWnd, None, solution=[800,600])
-
-
